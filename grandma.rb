8a9a5c82f1a701e6ee83c0d85af9a5f2f8e9ba6c
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-27 20:05:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@ require 'pry'
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  # binding.pry
+  binding.pry
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
-    ""
+    "HUH?! SPEAK UP, SONNY!"
   end
 end 
